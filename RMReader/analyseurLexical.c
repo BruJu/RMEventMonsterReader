6a3983f14518d82fdc7b38777f66bf5485dafbb2
BruJu <bruyat.ju@live.fr>
2017-06-16 00:21:22 +0200

    types.h et analyseur lexical
    Avancée légère de l'analyseur lexical qui est à refaire avec une nouvelle idée mise en commentaires

--- RMReader/analyseurLexical.c
+++ RMReader/analyseurLexical.c
@@ -1,6 +1,8 @@
+#include "types.h"
 
 char yyLine[255];
-
+Instruction yyInstruction;
+InstructionBis yyInstructionBis;
 
 /*
 
@@ -29,45 +31,6 @@ char yyLine[255];
 */
 
 
-typedef enum {
-    ChgSwitch,
-    ChgVariable,
-    Label,
-    JumpToLabel,
-    ForkIf,
-    ForkElse,
-    ForkEnd,
-    Loop,
-    LoopEnd,
-    LoopBreak,
-    ShowPicture,
-    ChangeItem,
-    Void
-} Instruction;
-
-typedef enum {
-    Egal, Different,
-    Plus, Moins, Fois, Divise, Modulo,
-    Inferieur, Superieur,
-    InfEgal, SupEgal
-} Signe;
-
-typedef union {
-    struct {
-        int numero;
-        Signe signe;
-        int nouvelleValeur;
-    } affectation;              // ChgSwitch, ChgVariable
-    int numero;                 // Label, JumpToLabel
-    /* TODO */ // ForkIf
-    struct {
-        int numero;
-        int quantite;
-    } changeItem;               // ChangeItem
-    char showPicture[255];      // ShowPicture
-    /* rien */ // ForkElse, ForkEnd, Loop, LoopEnd, LoopBreak, Void,
-} InstructionBis;
-
 /**
  *  Lit une ligne dans le fichier
  * 
@@ -90,6 +53,7 @@ int lireNouvelleLigne() {
     
     int positionChar = 0;
     
+    /* == Passer les espaces == */
     while (positionChar != 255 && yyLine[positionChar] == ' ') {
         positionChar++;
     }
@@ -97,25 +61,83 @@ int lireNouvelleLigne() {
     if (positionChar == 255)
         return 2;
     
+    /*
+     * TODO :
+     * Dans la mesure où les performances ne sont pas une question vitale pour ce projet,
+     * faire un dictionnaire avec la liste des modèles d'instructions
+     * faire un lecteur qui renvoie
+     * struct (instruction, chaine 1, chaine 2, chaine 3, chaine 4, ...)
+     * et une autre fonction s'occupe de convertir les chaines en instructionbis
+     * 
+     * Avantages :
+     * - Plus facilement maintenanble si on vuet ajouter des isntructions ou les modifier
+     * Défaut :
+     * - Redondance de traitements (négligeable car il n'y a qu'une vingtaine d'instruction
+     * et les programmes à traiter ne devraient pas dépasser les 100000 lignes même en voyant large)
+     */
+    
+        
     if (yyLine[positionChar] == '<') {
-        if (!(yyLine[positionChar+1] == '>' && yyLine[positionChar+2] == ' '))
+        /* == Instruction normale == */
+        if (!(yyLine[positionChar+1] == '>')
             return 2;
         
-        positionChar += 3;
+        positionChar += 2;
+        
+        if (yyLine[positionChar] == 0) {
+            // Instruction : <>
+            yyInstruction = Void;
+            return 0;
+        }
+        
+        if (!yyLine[positionChar] == ' ')
+            return 2;
+        
+        positionChar++;
+        
+        // <> Change Switch: [1] = ON
+        // <> Change Variable: [1] = 0
+        // <> Change Items: Add 1 piece of item #1
+        // <> Label: 1
+        // <> Jump To Label: 1
+        // <> Fork Condition: If Switch [1] == ON then ...
+        // <> Fork Condition: If Variable [10] != 0 then ...
+        // <> Loop
+        // <> Break Loop
+        // <> Show Picture: #1, actionslibre2, (160, 120), Mgn 100%, Tsp 0%/0%
         
         
         
     } else if (yyLine[positionChar] == ':') {
+        /* == End of something  == */
         if (!(yyLine[positionChar+1] == ' ' && yyLine[positionChar+2] == 'E'))
             return 2;
         
         positionChar += 3;
         
+        if (yyLine[positionChar] == 'l') {
+            // : Else ...
+            yyInstruction = ForkElse;
+            return 0;
+        }
         
+        // "nd of "  6 caractères
+        positionChar += 6;
         
-    } else {
+        if (yyLine[positionChar] == 'f') {
+            // : End of fork
+            yyInstruction = ForkEnd;
+            return 0;
+        } else if (yyLigne[positionChar == 'l') {
+            // : End of loop
+            yyInstruction == LoopEnd;
+            return 0;
+        }
+        
         return 2;
     }
+    
+    return 2;
     
 }
 
---
+++ RMReader/types.h
@@ -0,0 +1,76 @@
+#ifndef __H_TYPES__
+#define __H_TYPES__
+
+typedef enum {
+    ChgSwitch,
+    ChgVariable,
+    Label,
+    JumpToLabel,
+    ForkIf,
+    ForkElse,
+    ForkEnd,
+    Loop,
+    LoopEnd,
+    LoopBreak,
+    ShowPicture,
+    ChangeItem,
+    Void
+} Instruction;
+
+typedef enum {
+    Egal, Different,
+    Plus, Moins, Fois, Divise, Modulo,
+    Inferieur, Superieur,
+    InfEgal, SupEgal
+} Signe;
+
+typedef union {
+    struct {
+        int numero;
+        Signe signe;
+        int nouvelleValeur;
+    } affectation;              // ChgSwitch, ChgVariable
+    int numero;                 // Label, JumpToLabel
+    
+    struct {
+        int surSwitch;
+        union {
+            
+            
+        } contenu;
+    } forkIf;                   // ForkIf
+    
+    struct {
+        int numero;
+        int quantite;
+    } changeItem;               // ChangeItem
+    char showPicture[255];      // ShowPicture
+    /* rien */ // ForkElse, ForkEnd, Loop, LoopEnd, LoopBreak, Void,
+} InstructionBis;
+
+
+typedef struct {
+    int id;
+    char nom[100];
+} Entree;
+
+typedef struct {
+    int nombredEntrees;
+    int capacite;
+    Entree * entree;
+} Dictionnaire;
+
+
+typedef struct {
+    int id[50];
+    char nom[100];
+} EntreeChar;
+
+typedef struct {
+    int nombredEntrees;
+    int capacite;
+    EntreeChar * entree;
+} DictionnaireChar;
+
+
+#endif
