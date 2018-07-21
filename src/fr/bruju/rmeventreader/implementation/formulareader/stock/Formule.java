1f214a1670b15e0a7d63ba2dfcdbbd9aa897838d
Bruju <bruyat.ju@live.fr>
2018-07-21 05:04:57 +0200

    Polissage du code de stockage de formules

--- src/fr/bruju/rmeventreader/implementation/formulareader/stock/Formule.java
+++ src/fr/bruju/rmeventreader/implementation/formulareader/stock/Formule.java
@@ -2,18 +2,43 @@ package fr.bruju.rmeventreader.implementation.formulareader.stock;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import fr.bruju.rmeventreader.implementation.formulareader.formule.condition.Condition;
 import fr.bruju.rmeventreader.implementation.formulareader.formule.valeur.Valeur;
 import fr.bruju.rmeventreader.implementation.formulareader.model.PersonnageReel;
+import fr.bruju.rmeventreader.utilitaire.Triplet;
 
+/**
+ * Représentation d'une formule qui a été déduite du décrypteur de formules
+ * 
+ * @author Bruju
+ *
+ */
 public class Formule {
+	/**
+	 * Nom de l'attaque
+	 */
 	public final String nomAttaque;
+	/**
+	 * Variables ayant déclenché le stockage de la formule
+	 */
 	public final List<Integer> variableTouchee;
+	/**
+	 * Liste des conditions requises
+	 */
 	public final List<Condition> conditionsRequises;
+	/**
+	 * Formule
+	 */
 	public final Valeur formule;
 
+	/* ========================
+	 * CONSTRUCTION DE FORMULES
+	 * ======================== */
+
+	/**
+	 * Crée une formule simple
+	 */
 	public Formule(String nomAttaque, Integer variableTouchee, List<Condition> conditionsRequises, Valeur formule) {
 		this.nomAttaque = nomAttaque;
 		this.variableTouchee = new ArrayList<>();
@@ -22,18 +47,52 @@ public class Formule {
 		this.formule = formule;
 	}
 
-	public Formule(String nomAttaque, List<Integer> variableTouchee, List<Condition> conditionsRequises,
+	/* ===========
+	 * UNIFICATION
+	 * =========== */
+
+	/**
+	 * Crée une formule unifiée
+	 */
+	private Formule(String nomAttaque, List<Integer> variableTouchee, List<Condition> conditionsRequises,
 			Valeur formule) {
 		this.nomAttaque = nomAttaque;
-		this.variableTouchee = variableTouchee.stream().collect(Collectors.toList());
+		this.variableTouchee = variableTouchee;
 		this.conditionsRequises = conditionsRequises;
 		this.formule = formule;
 	}
 
+	/**
+	 * Crée une nouvelle formule en unifiant cette formule et le triplet v. A l'issue, cette formule n'est plus
+	 * utilisable.
+	 */
+	public Formule unifier(Triplet<Integer, List<Condition>, Valeur> v) {
+		String nomAttaque = this.nomAttaque;
+		
+		List<Integer> variablesTouchees = this.variableTouchee;
+		variablesTouchees.add(v.a);
+		
+		List<Condition> conditions = this.conditionsRequises;
+
+		Valeur valeurUnifiee = this.formule.similariser(v.c);
+		
+		return new Formule(nomAttaque, variablesTouchees, conditions, valeurUnifiee);
+	}
+
+	/* =========
+	 * AFFICHAGE
+	 * ========= */
+
+	/**
+	 * Renvoie une représentation de la formule
+	 */
 	public String getString() {
 		return getString(null);
 	}
 
+	/**
+	 * Renvoie une représentation de la formule en utilisant la fonction de simplification du personnage donné
+	 */
 	public String getString(PersonnageReel personnage) {
 		StringBuilder sb = new StringBuilder();
 
--- src/fr/bruju/rmeventreader/implementation/formulareader/stock/Stockage.java
+++ src/fr/bruju/rmeventreader/implementation/formulareader/stock/Stockage.java
@@ -12,19 +12,86 @@ import fr.bruju.rmeventreader.implementation.monsterlist.autotraitement.AutoActi
 import fr.bruju.rmeventreader.utilitaire.Pair;
 import fr.bruju.rmeventreader.utilitaire.Triplet;
 
+/**
+ * Base de données de formules et de personnages
+ * 
+ * @author Bruju
+ *
+ */
 public class Stockage {
+	/* ====
+	 * BASE
+	 * ==== */
+	
+	/**
+	 * Liste des personnages
+	 */
 	private List<PersonnageReel> personnages;
+	
+	/**
+	 * Liste des formules
+	 */
 	private List<Formule> formulesAcquises;
 
+	/**
+	 * Construction d'un stockage pour la liste des personnages donnés
+	 * 
+	 * @param personnages Les personnages qui seront concernés par les formules
+	 */
 	public Stockage(List<PersonnageReel> personnages) {
 		this.formulesAcquises = new ArrayList<>();
 		this.personnages = personnages;
 	}
+	
+	
+	/* =========
+	 * AFFICHAGE
+	 * ========= */
 
+	/**
+	 * Donne une liste de représentations textuelles des formules concernant le personnage donnné
+	 */
+	public List<String> getChaine(String nomDePersonnage, PersonnageReel personnage) {
+		return formulesAcquises.stream().filter(formule -> formule.formule.getString().contains(nomDePersonnage))
+				.map(formule -> formule.getString(personnage)).collect(Collectors.toList());
+	}
+
+	/* ========
+	 * SERVICES
+	 * ======== */
+	
+	/**
+	 * Decrypte des formules écrites dans la liste des fichiers donnés
+	 * 
+	 * @param listeDeFichiers Une liste d'association nom de fichier - attaque stockée dans le fichier
+	 */
 	public void remplir(List<Pair<String, String>> listeDeFichiers) {
 		listeDeFichiers.forEach(nomDeFichier -> remplir(nomDeFichier.getLeft(), nomDeFichier.getRight()));
 	}
+	
+	/**
+	 * Renvoie la liste des vrais personnages de la base (ie ceux qui n'ont ni Membre ni Monstre dans leur nom)
+	 * 
+	 * @return La liste des personnages
+	 */
+	public List<PersonnageReel> getVraiPersonnages() {
+		return personnages.stream().filter(p -> !p.getNom().contains("Membre"))
+				.filter(p -> !p.getNom().contains("Monstre")).sorted((p, q) -> p.getNom().compareTo(q.getNom()))
+				.collect(Collectors.toList());
+	}
 
+	/* ======================
+	 * FONCTIONNEMENT INTERNE
+	 * ====================== */
+
+	// Déchiffrage
+
+	/**
+	 * Décrypte la formule de dégâts de l'attaque donnée
+	 * 
+	 * @param nomDeFichier Le nom du fichier
+	 * @param nomAttaque Le nom de l'attaque
+	 */
 	private void remplir(String nomDeFichier, String nomAttaque) {
 		FormulaCalculator calc = new FormulaCalculator();
 		new AutoActionMaker(calc, nomDeFichier).faire();
@@ -33,6 +100,14 @@ public class Stockage {
 		valeursTrouvees.forEach(v -> ajouterUneFormule(nomAttaque, v));
 	}
 
+	// Ajout de formules
+	
+	/**
+	 * Ajoute une formule pour l'attaque donnée dans la base. Tente d'unifier des formules portant sur la même attaque
+	 * 
+	 * @param nomAttaque Nom de l'attaque
+	 * @param v Le triplet décrypté par l'action maker
+	 */
 	private void ajouterUneFormule(String nomAttaque, Triplet<Integer, List<Condition>, Valeur> v) {
 		if (formulesAcquises.isEmpty()) {
 			formulesAcquises.add(new Formule(nomAttaque, v.a, v.b, v.c));
@@ -42,38 +117,19 @@ public class Stockage {
 		Formule derniereFormuleEmpilee = formulesAcquises.get(formulesAcquises.size() - 1);
 
 		if (sontUnifiables(derniereFormuleEmpilee, v, nomAttaque)) {
-			unifier(derniereFormuleEmpilee, v);
+			formulesAcquises.set(formulesAcquises.size() - 1, derniereFormuleEmpilee.unifier(v));
 		} else {
 			formulesAcquises.add(new Formule(nomAttaque, v.a, v.b, v.c));
 		}
 	}
 
-	private void unifier(Formule derniereFormuleEmpilee, Triplet<Integer, List<Condition>, Valeur> v) {
-		formulesAcquises.remove(derniereFormuleEmpilee);
-		
-		String nomAttaque = derniereFormuleEmpilee.nomAttaque;
-		Valeur valeurUnifiee = derniereFormuleEmpilee.formule.similariser(v.c);
-		List<Integer> variablesTouchees = derniereFormuleEmpilee.variableTouchee;
-		variablesTouchees.add(v.a);
-		List<Condition> conditions = derniereFormuleEmpilee.conditionsRequises;
-		
-		formulesAcquises.add(new Formule(nomAttaque, variablesTouchees, conditions, valeurUnifiee));
-	}
-
+	// Unification
+	
+	/**
+	 * Retourne vrai si la formule et le triplet sont unifiables
+	 */
 	private boolean sontUnifiables(Formule derniereFormuleEmpilee, Triplet<Integer, List<Condition>, Valeur> v,
 			String nomAttaque) {
 		return derniereFormuleEmpilee.nomAttaque.equals(nomAttaque) && derniereFormuleEmpilee.formule.estSimilaire(v.c);
 	}
-
-	public List<PersonnageReel> getVraiPersonnages() {
-		return personnages.stream().filter(p -> !p.getNom().contains("Membre"))
-				.filter(p -> !p.getNom().contains("Monstre")).sorted((p, q) -> p.getNom().compareTo(q.getNom()))
-				.collect(Collectors.toList());
-	}
-
-	public List<String> getChaine(String nomDePersonnage, PersonnageReel personnage) {
-		return formulesAcquises.stream().filter(formule -> formule.formule.getString().contains(nomDePersonnage))
-				.map(formule -> formule.getString(personnage)).collect(Collectors.toList());
-	}
-
 }
