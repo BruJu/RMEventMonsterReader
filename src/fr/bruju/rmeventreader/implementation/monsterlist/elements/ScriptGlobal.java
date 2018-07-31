64133dcebb2b44196e62344aa39cdc4eefa996e8
Bruju <bruyat.ju@live.fr>
2018-07-31 12:51:52 +0200

    Appel des méthodes de la classe de base depuis les classes interne de l'analyse d'éléments

--- src/fr/bruju/rmeventreader/implementation/monsterlist/elements/ScriptGlobal.java
+++ src/fr/bruju/rmeventreader/implementation/monsterlist/elements/ScriptGlobal.java
@@ -25,6 +25,9 @@ import fr.bruju.rmeventreader.implementation.monsterlist.metier.Monstre;
 public class ScriptGlobal extends StackedActionMaker<Monstre> {
 	private MonsterDatabase bdd;
 	private ContexteElementaire contexte;
+	private ArrayList<ActionPage> actionsPage = new ArrayList<>();
+	private int ID_VARIABLE_MONSTRE_CIBLE = 552;
+	private int EVENT_SOUS_FONCTIONS = 99;
 
 	public ScriptGlobal(MonsterDatabase bdd, ContexteElementaire contexte) {
 		this.bdd = bdd;
@@ -53,7 +56,7 @@ public class ScriptGlobal extends StackedActionMaker<Monstre> {
 
 	@Override
 	public void callCommonEvent(int eventNumber) {
-		ScriptGeneral p = new ScriptGeneral(bdd, contexte);
+		ScriptGeneral p = new ScriptGeneral();
 
 		Interpreter interpreter = new Interpreter(p);
 
@@ -74,8 +77,18 @@ public class ScriptGlobal extends StackedActionMaker<Monstre> {
 
 		if (str.equals("REHAUSSER LES RESISTANCES")) {
 			// Fin - Application manuelle du calcul de réhaussement des résistances
-			bdd.extractMonsters().forEach(this::modifierMonstre);
+			bdd.extractMonsters().forEach(this::finaliserMonstre);
 		}
+	}
+
+	@Override
+	public boolean condOnVariable(int leftOperandValue, Operator operatorValue, ValeurFixe returnValue) {
+		if (leftOperandValue != ID_VARIABLE_MONSTRE_CIBLE) {
+			throw new LigneNonReconnueException("Script général : condition sur " + leftOperandValue);
+		}
+
+		this.conditions.push(new ConditionOnMonsterId(true, operatorValue, returnValue.valeur));
+		return true;
 	}
 
 	private void initialiserMonstre(Monstre monstre) {
@@ -83,10 +96,9 @@ public class ScriptGlobal extends StackedActionMaker<Monstre> {
 				new Donnees<Integer>(monstre, contexte.getElements(), 0, v -> v.toString()));
 		monstre.donnees.put(ContexteElementaire.PARTIES,
 				new Donnees<Boolean>(monstre, contexte.getParties(), false, v -> v ? "x" : "_"));
-
 	}
 
-	private void modifierMonstre(Monstre monstre) {
+	private void finaliserMonstre(Monstre monstre) {
 		Collection<String> elements = contexte.getElements();
 
 		int bonusCalc = monstre.accessInt(Monstre.STATS).get("Niveau");
@@ -100,89 +112,101 @@ public class ScriptGlobal extends StackedActionMaker<Monstre> {
 				element -> monstre.accessInt(ContexteElementaire.ELEMENTS).compute(element, (n, v) -> v - bonus));
 	}
 
-	public class ScriptGeneral extends StackedActionMaker<Monstre> {
-		private MonsterDatabase bdd;
-		private ContexteElementaire contexte;
-		private ArrayList<ActionPage> actionsPage = new ArrayList<>();
-		private int ID_VARIABLE_MONSTRE_CIBLE = 552;
-		private int EVENT_SOUS_FONCTIONS = 99;
-
-		public ScriptGeneral(MonsterDatabase bdd, ContexteElementaire contexte) {
-			this.bdd = bdd;
-			this.contexte = contexte;
+	@Override
+	public void callMapEvent(int eventNumber, int eventPage) {
+		if (eventNumber != EVENT_SOUS_FONCTIONS) {
+			throw new LigneNonReconnueException("Script général : appel d'évènement sur la carte sur " + eventNumber);
 		}
 
-		@Override
-		protected Collection<Monstre> getAllElements() {
-			return bdd.extractMonsters();
+		assurerExistance(eventPage);
+
+		this.getElementsFiltres().forEach(actionsPage.get(eventPage - 1)::appliquer);
+	}
+	
+	private void assurerExistance(int eventPage) {
+		while (actionsPage.size() <= eventPage) {
+			actionsPage.add(null);
 		}
+
+		if (actionsPage.get(eventPage - 1) == null) {
+			Page p = new Page();
+			Interpreter interpreter = new Interpreter(p);
+
+			try {
+				interpreter.inputFile(new File(ContexteElementaire.RESSOURCES_PREFIXE + eventPage + ".txt"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			actionsPage.set(eventPage - 1, p.getResult());
+		}
+	}
+	
+	
+	/* ==================
+	 * SOUS ACTION MAKERS
+	 * ================== */
+
+	/** Application des effets d'un appel à un évènement sur la carte */
+	public static interface ActionPage {
+		/** Applique les effets d'une page sur le monstre */
+		public void appliquer(Monstre monstre);
+	}
+
+	public class ScriptGeneral implements ActionMakerDefalse {
 
 		@Override
 		public void changeSwitch(Variable interrupteur, boolean value) {
-			String nom = contexte.getPartie(interrupteur.idVariable);
-
-			this.getElementsFiltres().forEach(monstre -> monstre.accessBool(PARTIES).set(nom, value));
+			ScriptGlobal.this.changeSwitch(interrupteur, value);
 		}
 
 		@Override
 		public void changeVariable(Variable variable, Operator operator, ValeurFixe returnValue) {
-			String nom = contexte.getElement(variable.idVariable);
-
-			this.getElementsFiltres().forEach(monstre -> monstre.accessInt(ELEMENTS).compute(nom,
-					(n, ex) -> operator.compute(ex, returnValue.valeur)));
+			ScriptGlobal.this.changeVariable(variable, operator, returnValue);
 		}
 
 		@Override
 		public boolean condOnVariable(int leftOperandValue, Operator operatorValue, ValeurFixe returnValue) {
-			if (leftOperandValue != ID_VARIABLE_MONSTRE_CIBLE) {
-				throw new LigneNonReconnueException("Script général : condition sur " + leftOperandValue);
-			}
-
-			this.conditions.push(new ConditionOnMonsterId(true, operatorValue, returnValue.valeur));
-			return true;
+			return ScriptGlobal.this.condOnVariable(leftOperandValue, operatorValue, returnValue);
 		}
 
 		@Override
 		public void callMapEvent(int eventNumber, int eventPage) {
-			if (eventNumber != EVENT_SOUS_FONCTIONS) {
-				throw new LigneNonReconnueException(
-						"Script général : appel d'évènement sur la carte sur " + eventNumber);
-			}
-
-			assurerExistance(eventPage);
-
-			this.getElementsFiltres().forEach(actionsPage.get(eventPage - 1)::appliquer);
+			ScriptGlobal.this.callMapEvent(eventNumber, eventPage);
 		}
 
-		private void assurerExistance(int eventPage) {
-			while (actionsPage.size() <= eventPage) {
-				actionsPage.add(null);
-			}
-
-			if (actionsPage.get(eventPage - 1) == null) {
-				Page p = new Page(contexte);
-				Interpreter interpreter = new Interpreter(p);
-
-				try {
-					interpreter.inputFile(new File(ContexteElementaire.RESSOURCES_PREFIXE + eventPage + ".txt"));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				actionsPage.set(eventPage - 1, p.getResult());
-			}
+		@Override
+		public void condElse() {
+			ScriptGlobal.this.condElse();
 		}
+
+		@Override
+		public void condEnd() {
+			ScriptGlobal.this.condEnd();
+		}
+
+
 
 	}
 
+	/**
+	 * Cette sous classe construit la liste des actions à réaliser lorsqu'une sous-fonction est appelée.
+	 * <p>
+	 * Le script appelle régulièrement des évènements qui se servent à appliquer des résistances liées à des types de
+	 * monstres. Cette classe lit les modifications faites et les donne sous la forme d'un objet à appliquer, ce qui
+	 * permet de gagner légèrement en performances.
+	 * <p>
+	 * Ces pages ne contiennent que des instruction du type "modifier une variable" ou "modifier des parties".
+	 *
+	 */
 	public class Page implements ActionMakerDefalse {
-		private ContexteElementaire contexte;
-
+		/** Liste des actions réaliser vues */
 		private List<Consumer<Monstre>> actionsARealiser = new ArrayList<>();
 
-		public Page(ContexteElementaire contexte) {
-			this.contexte = contexte;
-		}
-
+		/**
+		 * Donne le résultat de l'exécution
+		 * 
+		 * @return Une liste d'actions à réaliser sur un monstre qui appellerait la page
+		 */
 		public ActionPage getResult() {
 			return monstre -> actionsARealiser.forEach(action -> action.accept(monstre));
 		}
@@ -204,17 +228,13 @@ public class ScriptGlobal extends StackedActionMaker<Monstre> {
 
 		@Override
 		public void condElse() {
-			return;
+			// Pas de conditions
 		}
 
 		@Override
 		public void condEnd() {
-			return;
+			// Pas de conditions
 		}
 
-	}
-
-	public static interface ActionPage {
-		public void appliquer(Monstre monstre);
 	}
 }
