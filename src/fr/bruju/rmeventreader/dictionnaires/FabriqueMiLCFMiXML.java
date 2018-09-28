d40fa8497a11b7f1418bfc6869fabf62e5a6bfdd
Bruju <bruyat.ju@live.fr>
2018-09-28 18:46:46 +0200

    Documentation de Fabrique MiLCFMiXml

--- src/fr/bruju/rmeventreader/dictionnaires/FabriqueMiLCFMiXML.java
+++ src/fr/bruju/rmeventreader/dictionnaires/FabriqueMiLCFMiXML.java
@@ -13,6 +13,9 @@ import fr.bruju.rmeventreader.dictionnaires.liblcfreader.FabriqueCache;
 import fr.bruju.rmeventreader.filereader.FileReaderByLine;
 
 public class FabriqueMiLCFMiXML implements RMFabrique {
+	/* =========
+	 * SINGLETON
+	 * ========= */
 	
 	private static FabriqueMiLCFMiXML instance;
 
@@ -25,42 +28,30 @@ public class FabriqueMiLCFMiXML implements RMFabrique {
 		}
 		return instance;
 	}
+
+	/* =========
+	 * ATTRIBUTS
+	 * ========= */
 	
+	/** Fabrique LCF pour les maps */
 	private RMFabrique fabriqueLCF = new FabriqueLCF("ressources\\FichiersBruts");
+	/** Fabrique lisant à partir des fichiers issus des xml pour les évènements communs */
 	private RMFabrique fabriqueXML = FabriqueCache.getInstance();
-	
+	/** Redéfinition des noms */
 	private ArbreDeCartes arbre = new ArbreDeCartes();
 	
-	
-	private static class ArbreDeCartes {
-		private static class Element {
-			private int idPere;
-			private String nom;
-			
-			public Element(int idPere, String nom) {
-				this.idPere = idPere;
-				this.nom = nom;
-			}
-		}
-		
-		List<Element> elements = new ArrayList<>();
-		
-		public ArbreDeCartes() {
-			FileReaderByLine.lectureFichierRessources("ressources_gen\\bdd_maps.txt", ligne -> {
-				String[] elements = FileReaderByLine.splitter(ligne, 3);
-				
-				this.elements.add(new Element(Integer.parseInt(elements[1]), elements[2]));
-			});
-		}
-		
-		public String getChemin(int idMap) {
-			Element element = elements.get(idMap);
-			String prefixe = (idMap == 0) ? "" : getChemin(elements.get(idMap).idPere) + "-";
-			return prefixe + element.nom;
-		}
-		
+	/**
+	 * Décore la map lue dans les fichiers LCF avec le nom voulu
+	 * @param map La map à décorer
+	 * @return Une map comportant le nom issu de la redéfinition des noms
+	 */
+	private RMMap convertirMap(RMMap map) {
+		return new $Map(map, arbre.getChemin(map.id()));
 	}
-	
+
+	/* ========
+	 * FABRIQUE
+	 * ======== */
 	
 	@Override
 	public RMMap map(int idCarte) {
@@ -82,21 +73,25 @@ public class FabriqueMiLCFMiXML implements RMFabrique {
 		return fabriqueLCF.maps().stream().map(this::convertirMap).collect(Collectors.toList());
 	}
 	
-	private RMMap convertirMap(RMMap map) {
-		return new $Map(map, arbre.getChemin(map.id()));
-	}
-
 	@Override
 	public List<RMEvenementCommun> evenementsCommuns() {
 		return fabriqueXML.evenementsCommuns();
 	}
 	
-	
+	/**
+	 * Classe permettant de redéfinir le nom d'une map
+	 */
 	private class $Map implements RMMap {
-		
+		/** Map */
 		private RMMap map;
+		/** Nom de la map */
 		private String nom;
 
+		/**
+		 * Crée une map à partir d'une map déjà existante et d'un nouveau nom
+		 * @param map La map
+		 * @param nouveauNom Le nouveau nom
+		 */
 		public $Map(RMMap map, String nouveauNom) {
 			this.map = map;
 			this.nom = nouveauNom;
@@ -118,5 +113,58 @@ public class FabriqueMiLCFMiXML implements RMFabrique {
 		}
 	}
 
+	/* =========================
+	 * LECTURE DES NOUVEAUX NOMS 
+	 * ========================= */
 
+	/**
+	 * Classe englobant tous les noms à donner aux map
+	 */
+	private static class ArbreDeCartes {
+		/**
+		 * Un élément est un numéro de la map père et du nom de la map
+		 */
+		private static class Element {
+			/** Position du père dans le tableau */
+			private int idPere;
+			/** Nom de la map */
+			private String nom;
+			
+			/**
+			 * Construit un élément avec l'id du père et le nom de la map
+			 * @param idPere La position du père
+			 * @param nom Le nom de la map
+			 */
+			public Element(int idPere, String nom) {
+				this.idPere = idPere;
+				this.nom = nom;
+			}
+		}
+		
+		/** Liste des éléments connus */
+		private List<Element> elements = new ArrayList<>();
+		
+		/**
+		 * Crée l'arbre des cartes en lisant dans un fichier ressources nommés bdd_maps.txt
+		 */
+		public ArbreDeCartes() {
+			FileReaderByLine.lectureFichierRessources("ressources_gen\\bdd_maps.txt", ligne -> {
+				String[] elements = FileReaderByLine.splitter(ligne, 3);
+				
+				this.elements.add(new Element(Integer.parseInt(elements[1]), elements[2]));
+			});
+		}
+		
+		/**
+		 * Donne le chemin pour accéder à la map dans l'arborescence 
+		 * @param idMap Le numéro de la map
+		 * @return Une chaîne représentant le chemin pour aller à cette map sous la forme
+		 * <pre>Nom du Projet - Map 1 - (...) Map idMap</pre>
+		 */
+		public String getChemin(int idMap) {
+			Element element = elements.get(idMap);
+			String prefixe = (idMap == 0) ? "" : getChemin(elements.get(idMap).idPere) + "-";
+			return prefixe + element.nom;
+		}
+	}
 }
