1ac0c98bb7f6c3432789c5235105b415747ed13c
Bruju <bruyat.ju@live.fr>
2018-10-20 10:53:49 +0200

    Variables mortes par défaut, correction de la prise en compte de variables mortes en sortie de condition

--- src/fr/bruju/rmeventreader/implementation/detectiondeformules/simplifieurdalgorithmes/Simplifieur.java
+++ src/fr/bruju/rmeventreader/implementation/detectiondeformules/simplifieurdalgorithmes/Simplifieur.java
@@ -37,7 +37,7 @@ public class Simplifieur implements Runnable {
 			List<AttaqueALire> attaquesALire = ListeDesAttaques.extraireAttaquesALire();
 			//AttaqueALire.afficher(attaquesALire);
 			
-			AttaqueALire attaque = attaquesALire.get(12);
+			AttaqueALire attaque = attaquesALire.get(27);
 			System.out.println(attaque.nomAttaque + " " + attaque.nomPersonnage);
 			
 			Executeur executeur = new Executeur();
--- src/fr/bruju/rmeventreader/implementation/detectiondeformules/simplifieurdalgorithmes/inliner/DetecteurDeSimplifications.java
+++ src/fr/bruju/rmeventreader/implementation/detectiondeformules/simplifieurdalgorithmes/inliner/DetecteurDeSimplifications.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.sun.org.apache.bcel.internal.generic.Instruction;
 import fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.modele.algorithme.BlocConditionnel;
 import fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.modele.algorithme.InstructionAffectation;
 import fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.modele.algorithme.InstructionAffichage;
@@ -19,7 +20,12 @@ import fr.bruju.rmeventreader.utilitaire.Utilitaire;
 
 public class DetecteurDeSimplifications implements VisiteurDAlgorithme {
 
-	private Map<Integer, VivaciteVariable> vivacites = new HashMap<>();
+	//private Map<Integer, VivaciteVariable> vivacites = new HashMap<>();
+
+	private Map<Integer, InstructionGenerale> variablesVivantes = new HashMap<>();
+
+	private int nombreDiInstructionsVisitees = 0;
+	private int nombreDiInstructionsIgnorees = 0;
 
 	final Set<InstructionAffectation> instructionsAIgnorer = new HashSet<>(); // Mortes + inlinées
 	final Map<InstructionGenerale, List<InstructionAffectation>> affectationsInlinables = new HashMap<>();
@@ -31,21 +37,76 @@ public class DetecteurDeSimplifications implements VisiteurDAlgorithme {
 		
 		for (ExprVariable variable : variablesPresentes) {
 			int numeroDeCase = variable.idVariable;
-			Utilitaire.Maps.getX(vivacites, numeroDeCase, VivaciteVariable::new).faireVivre(instruction);
+
+			if (variablesVivantes.containsKey(numeroDeCase)) {
+				variablesVivantes.put(numeroDeCase, null);
+			} else {
+				variablesVivantes.put(numeroDeCase, instruction);
+			}
+
+			//Utilitaire.Maps.getX(vivacites, numeroDeCase, VivaciteVariable::new).faireVivre(instruction);
 		}
 	}
 
 
 	@Override
 	public void visit(InstructionAffectation instructionAffectation) {
+		nombreDiInstructionsVisitees++;
 		int numeroDeCase = instructionAffectation.variableAssignee.idVariable;
-		Utilitaire.Maps.getX(vivacites, numeroDeCase, VivaciteVariable::new).tuer(instructionAffectation, this);
+
+		if (!variablesVivantes.containsKey(numeroDeCase)) {
+			// Variable morte
+			instructionsAIgnorer.add(instructionAffectation);
+			nombreDiInstructionsIgnorees++;
+		} else {
+			InstructionGenerale utilisatrice = variablesVivantes.remove(numeroDeCase);
+
+			if (utilisatrice != null) {
+				// Inlinable
+				instructionsAIgnorer.add(instructionAffectation);
+				nombreDiInstructionsIgnorees++;
+				Utilitaire.Maps.ajouterElementDansListe(affectationsInlinables, utilisatrice, instructionAffectation);
+			}
+
+			noterExpression(instructionAffectation, instructionAffectation.expression);
+		}
+
+
+
+		//Utilitaire.Maps.getX(vivacites, numeroDeCase, VivaciteVariable::new).tuer(instructionAffectation, this);
 	}
 
 	@Override
 	public void visit(BlocConditionnel blocConditionnel) {
-		noterCondition(blocConditionnel, blocConditionnel.condition);
 
+		int differenceIgnoreesAvant = nombreDiInstructionsVisitees - nombreDiInstructionsIgnorees;
+
+		Map<Integer, InstructionGenerale> origine = this.variablesVivantes;
+		Map<Integer, InstructionGenerale> vrai = new HashMap<>(origine);
+		Map<Integer, InstructionGenerale> faux = new HashMap<>(origine);
+
+		variablesVivantes = vrai;
+		blocConditionnel.siVrai.acceptInverse(this);
+
+		variablesVivantes = faux;
+		blocConditionnel.siFaux.acceptInverse(this);
+
+
+		variablesVivantes = origine;
+
+		vrai.keySet().forEach(id -> variablesVivantes.put(id, null));
+		faux.keySet().forEach(id -> variablesVivantes.put(id, null));
+
+		int differenceIgnoreesApres = nombreDiInstructionsVisitees - nombreDiInstructionsIgnorees;
+
+		if (differenceIgnoreesAvant != differenceIgnoreesApres) {
+			noterCondition(blocConditionnel, blocConditionnel.condition);
+		}
+
+
+
+
+		/*
 		Deviation deviation = new Deviation(vivacites);
 
 		vivacites = deviation.filsVrai;
@@ -55,9 +116,12 @@ public class DetecteurDeSimplifications implements VisiteurDAlgorithme {
 		blocConditionnel.siFaux.acceptInverse(this);
 
 		vivacites = deviation.consolider();
+
+		*/
 	}
 
 	private void noterCondition(BlocConditionnel blocConditionnel, Condition condition) {
+		// TODO : la condition est vide, ne pas faire vivre les variables qu'elle utilise
 		if (condition instanceof ConditionVariable) { // La liste des conditions n'est pas amenée à évoluer
 			ConditionVariable cVariable = (ConditionVariable) condition;
 			
@@ -68,6 +132,7 @@ public class DetecteurDeSimplifications implements VisiteurDAlgorithme {
 
 	@Override
 	public void visit(InstructionAffichage instructionAffichage) {
+		nombreDiInstructionsVisitees++;
 		// Ignoré
 	}
 
