c1ae781bbf1c5a805dee14a74f878c45308e0718
Bruju <bruyat.ju@live.fr>
2018-10-23 16:05:17 +0200

    Lecture de plusieurs attaques

--- src/fr/bruju/rmeventreader/implementation/detectiondeformules/simplifieurdalgorithmes/Simplifieur.java
+++ src/fr/bruju/rmeventreader/implementation/detectiondeformules/simplifieurdalgorithmes/Simplifieur.java
@@ -10,7 +10,9 @@ import fr.bruju.rmeventreader.implementation.detectiondeformules.ListeDesAttaque
 import fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.bornage.Borneur;
 import fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.inliner.InlinerGlobal;
 import fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.modele.algorithme.Algorithme;
+import fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.modele.organisation.Formule;
 import fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.modele.organisation.Groupe;
+import fr.bruju.rmeventreader.utilitaire.Utilitaire;
 
 import static fr.bruju.rmeventreader.ProjetS.PROJET;
 
@@ -26,29 +28,55 @@ public class Simplifieur implements Runnable {
 	
 	@Override
 	public void run() {
-		Groupe groupeInitial = creerAlgorithmes();
-
-
-		Algorithme algorithme = createur.creerAlgorithme();
+		Groupe<String> groupeInitial = creerAlgorithmes();
 		
 		for (Simplification simplification : simplifications) {
-			algorithme = simplification.simplifier(algorithme);
+			groupeInitial.simplifier(simplification);
 		}
 
-		System.out.println(algorithme.getString());
+		System.out.println(groupeInitial.getString());
 	}
 
 	private Groupe creerAlgorithmes() {
-		List<AttaqueALire> attaquesALire = ListeDesAttaques.extraireAttaquesALire();
-		Map<String, Groupe> personnages = new HashMap<>();
+		Map<String, Map<String, Algorithme>> attaquesDesPersonnages = extraireAlgorithmesDesPersonnages();
+		return transformerEnGroupes(attaquesDesPersonnages);
+	}
 
-		for (AttaqueALire attaque : attaquesALire) {
+	private static Groupe<String> transformerEnGroupes(Map<String, Map<String, Algorithme>> attaquesDesPersonnages) {
+		Groupe<String> groupeRacine = new Groupe("Personnage", new HashMap<>());
 
+		for (Map.Entry<String, Map<String, Algorithme>> personnage : attaquesDesPersonnages.entrySet()) {
+			String nomPersonnage = personnage.getKey();
+			Map<String, Algorithme> attaques = personnage.getValue();
 
+			Groupe groupe = new Groupe("Attaque", attaques);
 
+			for (Map.Entry<String, Algorithme> algorithmes : attaques.entrySet()) {
+				groupe.ajouter(algorithmes.getKey(), new Formule(algorithmes.getValue()));
+			}
+
+			groupeRacine.ajouter(nomPersonnage, groupe);
 		}
 
-		return new Groupe(null, new ArrayList<>(personnages.values()));
+		return groupeRacine;
+	}
+
+	private static Map<String, Map<String, Algorithme>> extraireAlgorithmesDesPersonnages() {
+		List<AttaqueALire> attaquesALire = ListeDesAttaques.extraireAttaquesALire();
+		Map<String, Map<String, Algorithme>> personnages = new HashMap<>();
+
+		for (AttaqueALire attaque : attaquesALire) {
+			Executeur executeur = new Executeur();
+			PROJET.lireEvenementCommun(executeur, attaque.numeroEvenementCommun);
+			Algorithme algorithme = executeur.extraireAlgorithme();
+
+			Map<String, Algorithme> attaquesDuPersonnage =
+					Utilitaire.Maps.getX(personnages, attaque.nomPersonnage, HashMap::new);
+
+			attaquesDuPersonnage.put(attaque.nomAttaque, algorithme);
+		}
+
+		return personnages;
 	}
 
 	private static class Initiateur implements CreateurDAlgorithme {
--- src/fr/bruju/rmeventreader/implementation/detectiondeformules/simplifieurdalgorithmes/modele/organisation/Groupe.java
+++ src/fr/bruju/rmeventreader/implementation/detectiondeformules/simplifieurdalgorithmes/modele/organisation/Groupe.java
@@ -1,16 +1,42 @@
 package fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.modele.organisation;
 
+import fr.bruju.rmeventreader.implementation.detectiondeformules.simplifieurdalgorithmes.Simplification;
+
 import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
-public class Groupe implements Element {
-	private final Classifieur classifieur;
-	private List<Element> fils;
+public class Groupe<Classifieur> implements Element {
+	private final String nom;
+	private final Map<Classifieur, Element> elements;
 
-	public Groupe(Classifieur classifieur, List<Element> fils) {
-		this.classifieur = classifieur;
-		this.fils = fils;
+	public Groupe(String nom, Map<Classifieur, Element> elements) {
+		this.nom = nom;
+		this.elements = elements;
 	}
 
+	public void ajouter(Classifieur classifieur, Element element) {
+		elements.put(classifieur, element);
+	}
 
+	@Override
+	public String getString(Stack<String> categories) {
+		StringBuilder sb = new StringBuilder();
 
+		for (Map.Entry<Classifieur, Element> classifieur : elements.entrySet()) {
+			System.out.println("CLASSIFIEUR : " + classifieur.toString());
+			categories.push(classifieur.getKey().toString());
+			sb.append(classifieur.getValue().getString(categories));
+			categories.pop();
+		}
+
+		return sb.toString();
+	}
+
+	@Override
+	public void simplifier(Simplification simplification) {
+		for (Element element : elements.values()) {
+			element.simplifier(simplification);
+		}
+	}
 }
