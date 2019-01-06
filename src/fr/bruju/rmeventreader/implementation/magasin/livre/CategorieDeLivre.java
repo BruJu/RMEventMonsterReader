4b00559337fc6b8bca80631e0d56e3a3e30e2c7d
Bruju <bruyat.ju@live.fr>
2019-01-06 15:05:36 +0100

    Doc catégorie de livre

--- src/fr/bruju/rmeventreader/implementation/magasin/livre/CategorieDeLivre.java
+++ src/fr/bruju/rmeventreader/implementation/magasin/livre/CategorieDeLivre.java
@@ -6,19 +6,31 @@ import fr.bruju.rmeventreader.implementation.magasin.objet.Livre;
 import java.util.*;
 
 /**
- * Regroupe des livres augmentant la même statistique
+ * Cette classe donne pour chaque livre qu'elle contient la liste des magasins qui le vend au prix le moins élevé.
+ * Les livres contenus sont considérés comme augmentant la statistique augmentée donnée lors de la création de la
+ * classe.
  */
 public class CategorieDeLivre {
+	/** Statistique augmentée par tous les livres contenus dans cette catégorie */
 	public final Livre.StatistiqueDeLivre statistiqueAugmentee;
 
 	/** Liste des livres achetables et les magasins où ils sont disponibles */
 	private Map<Livre, List<Magasin>> livresAchetables;
 
+	/**
+	 * Crée une catégorie de livres
+	 * @param statistiqueAugmentee La statistique augmentée par tous les livres qui seront contenus
+	 */
 	public CategorieDeLivre(Livre.StatistiqueDeLivre statistiqueAugmentee) {
 		this.statistiqueAugmentee = statistiqueAugmentee;
 		livresAchetables = new TreeMap<>();
 	}
 
+	/**
+	 * Ajoute un livre à cette catégorie
+	 * @param livre Le livre
+	 * @param magasin Le magasin vendant le livre
+	 */
 	public void ajouterLivre(Livre livre, Magasin magasin) {
 		if (!livresAchetables.containsKey(livre)) {
 			livresAchetables.put(livre, new ArrayList<>());
@@ -33,10 +45,22 @@ public class CategorieDeLivre {
 		zonesDAchat.add(magasin);
 	}
 
+	/**
+	 * Donne le nombre de livres
+	 * @return Le nombre de livres
+	 */
 	public int nombreDeLivres() {
 		return livresAchetables.keySet().size();
 	}
 
+	/**
+	 * Donne un affichage de la catégorie
+	 * @return Un affichage du type
+	 * <pre>
+	 *      x Statistique
+	 *      - IdLibre : NomLivre [VariationDePrix ; Lieux des magasins]
+	 * </pre>
+	 */
 	public String getAffichage() {
 		StringBuilder sb = new StringBuilder();
 
