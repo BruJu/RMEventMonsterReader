ee972fb3d292419e2b2521c074eb5fa812c4c775
Bruju <bruyat.ju@live.fr>
2019-01-07 14:08:23 +0100

    Doc Magasin

--- src/fr/bruju/rmeventreader/implementation/magasin/Magasin.java
+++ src/fr/bruju/rmeventreader/implementation/magasin/Magasin.java
@@ -5,13 +5,27 @@ import fr.bruju.rmeventreader.implementation.magasin.objet.Objet;
 import java.util.Set;
 import java.util.TreeSet;
 
+/**
+ * Représente un magasin dans le jeu
+ */
 public class Magasin {
-	private int idMagasin;
+	/** ID du magasin */
+	public final int idMagasin;
+	/** Arborescence menant à ce magasin */
 	private String cheminMap;
+	/** Niveau minimal pour faire un hold up */
 	private int niveauHoldup;
+	/** Liste des objets vendus */
 	private Set<Objet> objets;
+	/** Variation de prix du magasin */
 	public final int variationPrix;
-	
+
+	/**
+	 * Crée un magasin
+	 * @param idMagasin ID du magasin
+	 * @param cheminMap Emplacement du magasin
+	 * @param variationPrix Variation de prix du magasin
+	 */
 	public Magasin(int idMagasin, String cheminMap, int variationPrix) {
 		this.idMagasin = idMagasin;
 		this.cheminMap = cheminMap;
@@ -33,43 +47,27 @@ public class Magasin {
 		objets = new TreeSet<>();
 	}
 
-	public int getId() {
-		return idMagasin;
-	}
-	
+	/**
+	 * Ajoute l'objet à la liste des objets vendus par ce magasin
+	 * @param objet L'objet à ajouter
+	 */
 	public void ajouterObjet(Objet objet) {
 		objets.add(objet);
 	}
-	
+
+	/**
+	 * Modifie le niveau pour faire un hold up
+	 * @param niveau Le niveau pour faire un hold up
+	 */
 	public void setNiveauHoldup(int niveau) {
 		this.niveauHoldup = niveau;
 	}
 	
-	public String getMagasinCompact() {
-		return cheminMap + " @ " + niveauHoldup;
-	}
-	
-	public String getMagasinComplet() {
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append("> ")
-		  .append(cheminMap)
-		  .append("\n")
-		  .append("ID : ")
-		  .append(idMagasin)
-		  .append("\n")
-		  .append("Niveau :")
-		  .append(niveauHoldup)
-		  .append("\n")
-		  .append("Objets :");
-		
-		objets.forEach(objet -> sb.append("\n").append(objet.getString()));
-		
-		sb.append("\n");
-		
-		return sb.toString();
-	}
-
+	/**
+	 * Renvoie vrai si le magasin vend l'objet dont le numéro est donné
+	 * @param idObjet Le numéro de l'objet
+	 * @return Vrai si le magasin vend un objet contenant cet id.
+	 */
 	public boolean possedeLObjetdID(int idObjet) {
 		for (Objet objet : objets) {
 			if (objet.id == idObjet) {
@@ -80,18 +78,68 @@ public class Magasin {
 		return false;
 	}
 
+	/**
+	 * Donne le lieu où se trouve le magasin
+	 * @return Le lieu où se trouve le magasin
+	 */
 	public String getLieu() {
 		return cheminMap;
 	}
 
+	/**
+	 * Permet de savoir si des objets sont vendus
+	 * @return Vrai si au moins un objet est vendu
+	 */
 	public boolean vendUnObjet() {
 		return !objets.isEmpty();
 	}
 
+	/**
+	 * Donne la liste des objets vendus
+	 * @return La liste des objets vendus
+	 */
 	public Iterable<Objet> objetsVendus() {
 		return objets;
 	}
 
+
+	/**
+	 * Donne une représentation avec seulement le lieu du magasin et le niveau pour le voler
+	 * @return <code>cheminMap + " @ " + niveauHoldup;</code>
+	 */
+	public String getMagasinCompact() {
+		return cheminMap + " @ " + niveauHoldup;
+	}
+
+	/**
+	 * Donne une représentation complète du magasin sur plusieurs lignes
+	 */
+	public String getMagasinComplet() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("> ")
+				.append(cheminMap)
+				.append("\n")
+				.append("ID : ")
+				.append(idMagasin)
+				.append("\n")
+				.append("Niveau :")
+				.append(niveauHoldup)
+				.append("\n")
+				.append("Objets :");
+
+		objets.forEach(objet -> sb.append("\n").append(objet.getString()));
+
+		sb.append("\n");
+
+		return sb.toString();
+	}
+
+	/**
+	 * Transforme la variation de prix en un pourcentage
+	 * @param variableDeBase La variation déclarée dans le jeu
+	 * @return La variation représentée sous forme de pourcentage x 100
+	 */
 	public static int transformerVariation(int variableDeBase) {
 		switch (variableDeBase) {
 			case 0:
